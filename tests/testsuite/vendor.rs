2f83c7303017547d927d4c7373bb374ce38f70d7
Lizhang <v.kzhang@qq.com>
2025-11-06 22:44:07 +0800

    fix(command-vendor): reproduce the panic of cp_sources

--- tests/testsuite/vendor.rs
+++ tests/testsuite/vendor.rs
@@ -2086,3 +2086,43 @@ Caused by:
 "#]])
         .run();
 }
+
+#[cargo_test(ignore_windows = "annoying to create a repro on Windows; see rust-lang/cargo#16214")]
+fn vendor_rename_fallback() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "foo"
+                version = "0.1.0"
+
+                [dependencies]
+                log = "0.3.5"
+            "#,
+        )
+        .file("src/lib.rs", "")
+        .build();
+
+    Package::new("log", "0.3.5").publish();
+
+    // Create vendor/log as a file to force rename failure
+    let vendor_dir = p.root().join("vendor");
+    fs::create_dir_all(&vendor_dir).unwrap();
+    fs::write(vendor_dir.join("log"), "blocking file").unwrap();
+
+    p.cargo("vendor --respect-source-config --no-delete")
+        .env("CARGO_LOG", "cargo::ops::vendor=warn")
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+...
+[..]failed to `mv "[..]vendor[..].vendor-staging[..]log-0.3.5" "[..]vendor[..]log"`: [..]
+...
+[..]StripPrefixError[..]
+...
+"#]])
+        .run();
+
+    // The cp_sources fallback didn't work because of the StripPrefixError
+    assert!(!p.root().join("vendor/log/Cargo.toml").exists());
+}
